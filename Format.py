9349512476088042489b23b032482b8f3eacc23f
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2018-05-14 20:15:05 +0100

    Create Format.py

---
+++ Format.py
@@ -0,0 +1,10 @@
+wordList = open('./Lyrics', 'r').readlines()
+newWord = []
+for word in wordList:
+    if word.lower().endswith('\n'):
+        newWord.append(str(word)[:-2])
+    else:
+        newWord.append(str(word))
+f = open('./NewLyrics.txt', 'w+')
+joinedStr = ' '.join(newWord)
+f.write(joinedStr.lower().replace('!', '').replace('(', '').replace(')', '').replace('.', '').replace(',', '').replace('\'', '').replace('\"', ''))
